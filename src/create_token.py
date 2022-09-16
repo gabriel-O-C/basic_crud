60672ebaee447677d5b607ce4b502382690e9c55
gabriel de oliveira chaves <gabrieloliveirachaves754@gmail.com>
2022-09-16 18:18:42 -0300

    Add files via upload

---
+++ src/create_token.py
@@ -0,0 +1,20 @@
+import requests
+
+
+url = 'http://127.0.0.1:3001/tokens'
+
+user_data = {
+  'password': 'senha valida',
+  'email': 'mail2@gmail.com'
+}
+
+response = requests.post(url=url, json=user_data)
+
+if response.ok:
+  response_data = response.json()
+  token = response_data['token']
+  with open('.env', 'w') as file:
+    file.write(token)
+else:
+  print(response.status_code)
+  print(response.text)
